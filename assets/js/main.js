b42bd8b9933f5cad8df988c15ad5cc5a167aa0d1
frankjram <frankjunior23@gmail.com>
2025-07-22 22:15:07 -0400

    atualizando informações dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,31 @@
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job1 = document.getElementById('profile.job1')
+    job1.innerText = profileData.job1
+
+    const job2 = document.getElementById('profile.job2')
+    job2.innerText = profileData.job2
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+
+    const github = document.getElementById('profile.github')
+    github.innerText = profileData.github
+}
+
 
 (async () => {
     
